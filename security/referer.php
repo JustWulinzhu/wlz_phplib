873b3cc37fc4a25753b33ce279fe95a0040d97ef
wulinzhu <linzhu.wu@beebank.com>
2019-11-01 19:32:02 +0800

    refer

---
+++ security/referer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: wulinzhu
+ * Date: 19/11/1 下午6:16
+ * Email: 18515831680@163.com
+ */
+
+require_once dirname(__DIR__) . '/' . 'fun.php';
+
+class Referer {
+
+    public function checkDomain() {
+
+    }
+
+}
