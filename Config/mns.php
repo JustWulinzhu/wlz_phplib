8931407a170d90536067b88fd32d4db510d86c18
wulinzhu <linzhu.wu@beebank.com>
2019-11-07 11:12:06 +0800

    处理多余小写文件

--- Config/mns.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'mns' => array(
-        'access_id' => "LTAI4FkP13EDesv9dXV9cTX3",
-        'access_key' => "laqGqWCd0gstk5Rc8MasDGnzkIOyLg",
-        'end_point' => "http://1154287063650130.mns.cn-beijing-internal.aliyuncs.com",
-    ),
-);
