37043c5dac1d166041900a05a1c4aa9a084f0809
wulinzhu <linzhu.wu@beebank.com>
2020-05-28 16:06:47 +0800

    GuzzleHTTP

--- S/Http/Guzzle.php
+++ S/Http/Guzzle.php
@@ -7,8 +7,24 @@
  *
  * Guzzle HTTP请求
  *
+ * @query array
+ *      get参数
+ *
+ * @form_params array
+ *      post参数
+ *
+ * @headers array
+ *      header头信息
+ *
+ * @proxy string
+ *      代理信息
+ *
+ * @upload_file @body resources
+ *      要上传的文件内容
+ *
  * demo:
  * \S\Http\Guzzle::request('http://wlfeng.vip/test/index', 'GET');
+ * \S\Http\Guzzle::request('http://wlfeng.vip/test/index', 'POST', ['form_params' => ['name' => '张三'], 'upload_file' => file_get_contents('/tmp/a.txt')]);
  *
  */
 
@@ -52,6 +68,9 @@ class Guzzle
         if ($proxy) {
             $options['proxy'] = $proxy;
         }
+        if (isset($params['upload_file'])) {
+            $options['body'] = $params['upload_file'];
+        }
 
         Log::getInstance()->debug([__METHOD__, 'request params', $method, $url, json_encode($options)]);
         $response = $client->request($method, $url, $options);
@@ -59,7 +78,7 @@ class Guzzle
         Log::getInstance()->debug([__METHOD__, 'response params', $result]);
 
         if (self::HTTP_SUCCESS_CODE != ($error_code = $response->getStatusCode())) {
-            throw new \Exception("response error code {$error_code}");
+            throw new \Exception($response->getReasonPhrase(), $error_code);
         }
         if (empty($result)) {
             throw new \Exception("empty response from {$url}");
