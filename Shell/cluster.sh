e21608c4f5b788c01729939a16d309da5084f4e1
wulinzhu <linzhu.wu@beebank.com>
2021-07-14 18:12:11 +0800

    redis集群shell脚本

--- Shell/cluster.sh
+++ Shell/cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # redis集群启动、停止、重启脚本
+# 所有节点启动之后然后进行创建集群 ps: redis-cli --cluster create 127.0.0.1:6382 127.0.0.1:6383 127.0.0.1:6384 127.0.0.1:6385 127.0.0.1:6386 127.0.0.1:6387 --cluster-replicas 1
 
 if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ -z $1 ];
 then
@@ -9,32 +10,59 @@ then
     exit;
 fi;
 
-path_arr=(
-  "/usr/local/redis/cluster/6382/redis.conf"
-  "/usr/local/redis/cluster/6383/redis.conf"
-  "/usr/local/redis/cluster/6384/redis.conf"
-  "/usr/local/redis/cluster/6385/redis.conf"
-  "/usr/local/redis/cluster/6386/redis.conf"
-  "/usr/local/redis/cluster/6387/redis.conf"
+#redis根目录
+redis_path="/usr/local/redis/";
+
+#redis端口节点数组
+redis_port_arr=(
+    "6382"
+    "6383"
+    "6384"
+    "6385"
+    "6386"
+    "6387"
 );
 
-function start() {
-    stop;
+#创建集群配置文件目录
+if [ ! -d $redis_path"cluster" ]; then
+  mkdir $redis_path"cluster";
+fi
 
-    for path in ${path_arr[@]};
-    	  do
-        /usr/local/redis/cluster/redis-server ${path} >/dev/null 2>&1;
-    done
-}
+#创建集群配置文件，并且修改各文件相关配置
+for port in ${redis_port_arr[@]};
+    do
+      if [ ! -f $redis_path"cluster/redis_${port}.conf" ]; then
+          sed -e "98 s/6379/${port}/" -e "1393 s/#//" -e "1393 s/6379/${port}/" $redis_path"redis.conf" >> $redis_path"cluster/redis_${port}.conf";
+      fi
+done
 
+#停止所有redis
 function stop() {
     ps aux | grep redis | grep -v grep | awk -F " " '{print $2}' | xargs kill -9 >/dev/null 2>&1;
 }
 
+#启动redis节点及集群配置
+function start() {
+    stop;
+
+    for redis_conf in `ls $redis_path"cluster"`;
+    	  do
+        /usr/local/redis/redis-server $redis_path"cluster/"$redis_conf >/dev/null 2>&1;
+    done
+
+    for port in ${redis_port_arr[@]}; do
+        hosts=$hosts" 127.0.0.1:"$port" "
+    done
+
+    $redis_path"redis-cli" " --cluster create "$hosts" --cluster-replicas 1" >/dev/null 2>&1;
+}
+
+#redis重启
 function reload() {
     start;
 }
 
+#redis状态查询
 function status() {
     ps aux | grep redis | grep -v grep;
 }
