cf53726c9c21e3e6d56868f5d187b085368eb985
wulinzhu <18515831680@163.com>
2022-06-28 17:52:49 +0800

    'video'

--- App/Controller/Mooc/Dologin.php
+++ App/Controller/Mooc/Dologin.php
@@ -5,13 +5,12 @@ namespace App\Controller\Mooc;
 use S\Log;
 use S\Queue\Redis\Redis;
 
-class DoLogin extends \App\Controller\Base {
+class Dologin extends \App\Controller\Base {
 
     public $verify = false;
 
     public function index($args)
     {
-        $this->response_format = self::RESPONSE_FORMAT_JSON;
         dd($_POST);
     }
 
