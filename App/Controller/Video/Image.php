12768663bb950d534c1d13ac8d34692d1b36e5ef
wulinzhu <18515831680@163.com>
2022-06-29 17:31:37 +0800

    'video'

--- App/Controller/Video/Image.php
+++ App/Controller/Video/Image.php
@@ -4,6 +4,7 @@ namespace App\Controller\Video;
 
 use S\Log;
 use S\Queue\Redis\Redis;
+use S\Tools;
 
 class Image extends \App\Controller\Base {
 
@@ -11,9 +12,25 @@ class Image extends \App\Controller\Base {
 
     public function index($args)
     {
+        $ip = Tools::getCliIp();
         $content = file_get_contents("/tmp/video.txt");
+
+        $flag = 'unlooked';
+        if (file_exists("/tmp/video_record_{$ip}.txt")) {
+            $video_record = file_get_contents("/tmp/video_record_{$ip}.txt");
+            if ($content == $video_record) {
+                $flag = 'looked';
+            } else {
+                file_put_contents("/tmp/video_record_{$ip}.txt", $content);
+            }
+        } else {
+            file_put_contents("/tmp/video_record_{$ip}.txt", $content);
+            chmod("/tmp/video_record_{$ip}.txt", 0777);
+        }
+
         define("IMAGE_URL", $content);
         define("DES", "TEST");
+        define("IS_LOOKED", $flag);
         $this->smarty->display("Video/Index.html");
     }
 
