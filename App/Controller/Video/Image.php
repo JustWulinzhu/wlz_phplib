59508367e6d698d58723a44a592cae2c1c3d3f88
wulinzhu <18515831680@163.com>
2022-06-29 16:46:57 +0800

    'video'

--- App/Controller/Video/Image.php
+++ App/Controller/Video/Image.php
@@ -11,8 +11,8 @@ class Image extends \App\Controller\Base {
 
     public function index($args)
     {
-        define("MP4_URL", "http://wlfeng.vip:8081/image/test.mp4");
-        define("IMAGE_URL", "http://www.wlfeng.vip:8081/image/gou.png");
+        $content = file_get_contents("/tmp/video.txt");
+        define("IMAGE_URL", $content);
         define("DES", "TEST");
         $this->smarty->display("Video/Index.html");
     }
