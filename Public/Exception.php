33df620f9b9e9acb9d4b5ad5cec0cf5aaf458cdf
wulinzhu <18515831680@163.com>
2022-09-16 18:13:21 +0800

    '顶级异常处理'

--- Public/Exception.php
+++ Public/Exception.php
@@ -3,11 +3,11 @@
  * 自定义顶层异常类，所有未捕获的异常都会在这里被捕获
  * @param $exception
  */
-function myException($exception)
+function finalException($exception)
 {
     echo "<b>Uncaught Exception：</b> " . $exception->getMessage() . "<br/>";
     echo "<b>Uncaught Exception File：</b> " . $exception->getFile() . "<b> in line </b>" . $exception->getLine();
     \S\Log::getInstance()->debug(["Uncaught Exception：" . $exception->getMessage() . " | " . "Uncaught Exception File：" . $exception->getFile() . " in line " . $exception->getLine()], 'exception');
 }
 
-set_exception_handler('myException');
+set_exception_handler('finalException');
